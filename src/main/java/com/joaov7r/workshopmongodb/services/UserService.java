987da6fdad5d7abd519c740eb358b1e60f9d76d1
João Vitor <jvc.lopes@outlook.com>
2024-09-16 16:19:32 -0300

    User insertion with POST

--- src/main/java/com/joaov7r/workshopmongodb/services/UserService.java
+++ src/main/java/com/joaov7r/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaov7r.workshopmongodb.domain.User;
+import com.joaov7r.workshopmongodb.dto.UserDTO;
 import com.joaov7r.workshopmongodb.repository.UserRepository;
 import com.joaov7r.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRep.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return userRep.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

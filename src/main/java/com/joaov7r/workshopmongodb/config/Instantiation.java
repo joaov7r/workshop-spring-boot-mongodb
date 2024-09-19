e702f057251d1531ed7cff5e09d2a1093caf2cab
João Vitor <jvc.lopes@outlook.com>
2024-09-19 14:30:19 -0300

    Referencing User's posts

--- src/main/java/com/joaov7r/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/joaov7r/workshopmongodb/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner{
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
 
 		postRepository.saveAll(Arrays.asList(p1, p2));
+		
+		u1.getPosts().addAll(Arrays.asList(p1, p2));
+		userRepository.save(u1);
 	}
 
 }
--- src/main/java/com/joaov7r/workshopmongodb/domain/User.java
+++ src/main/java/com/joaov7r/workshopmongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.joaov7r.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)//Ensures that posts are only loaded when necessary
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -48,6 +54,15 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override

2cc618b6ce46f8a23a507025266b5aff993cb047
João Vitor <jvc.lopes@outlook.com>
2024-09-23 13:52:23 -0300

    Query with various criteria

--- src/main/java/com/joaov7r/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/joaov7r/workshopmongodb/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.joaov7r.workshopmongodb.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,20 @@ public class PostResource {
 	
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
-		
 		text = URL.decodeParam(text);
 		List<Post> list =service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullseacrh(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list =service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
